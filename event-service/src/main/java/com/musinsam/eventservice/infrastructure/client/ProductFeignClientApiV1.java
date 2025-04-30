47c908366eab97317ca788dd8283137ad3f5e3c0
Sohae Park <ipnsohe@naver.com>
2025-05-01 02:34:52 +0900

    feat: 이벤트 상품 레디스에 저장, 수정, 삭제 기능 연동

--- event-service/src/main/java/com/musinsam/eventservice/infrastructure/client/ProductFeignClientApiV1.java
+++ event-service/src/main/java/com/musinsam/eventservice/infrastructure/client/ProductFeignClientApiV1.java
@@ -1,11 +1,15 @@
 package com.musinsam.eventservice.infrastructure.client;
 
 import com.musinsam.eventservice.application.integration.ProductClient;
+import com.musinsam.eventservice.infrastructure.dto.req.ReqProductSaveProductsDtoApiV1;
 import com.musinsam.eventservice.infrastructure.dto.res.ResProductInfoGetByProductId;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "product-service")
 public interface ProductFeignClientApiV1 extends ProductClient {
@@ -13,5 +17,18 @@ public interface ProductFeignClientApiV1 extends ProductClient {
   // 상품 정보
   @GetMapping("/internal/v1/products/{productId}")
   ResProductInfoGetByProductId getProductInfo(@PathVariable UUID productId);
+
+  // 레디스에 이벤트 상품 저장
+  @PostMapping("/internal/v1/products/save")
+  void saveProductsToRedis(@RequestBody ReqProductSaveProductsDtoApiV1 dto);
+
+  // 할인율 수정
+  @PostMapping("/internal/v1/products/update-discount-rate")
+  void updateDiscountRate(@RequestParam UUID productId,
+      @RequestParam Integer discountRate);
+
+  // 이벤트 상품 삭제
+  @PostMapping("/internal/v1/products/delete")
+  void deleteEventProduct(@RequestParam UUID productId);
 
 }
---
+++ product-service/src/main/java/com/musinsam/productservice/infrastructure/dto/res/ReqProductSaveProductsDtoApiV1.java
@@ -0,0 +1,29 @@
+package com.musinsam.productservice.infrastructure.dto.res;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReqProductSaveProductsDtoApiV1 {
+
+  private List<EventProduct> eventProductList;
+  private LocalDateTime endTime;
+
+  @Builder
+  @Getter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class EventProduct {
+
+    private UUID productId;
+    private Integer discountRate;
+  }
+}
