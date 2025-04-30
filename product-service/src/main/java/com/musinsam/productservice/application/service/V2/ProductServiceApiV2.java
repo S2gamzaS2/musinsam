579d24b552f1a03a6c280d060fa242ef192af371
Sohae Park <ipnsohe@naver.com>
2025-05-01 02:42:08 +0900

    feat/refactor: V2, 상품 캐싱 처리 및 중복 코드 유틸리티 메서드로 리팩토링

---
+++ product-service/src/main/java/com/musinsam/productservice/application/service/V2/ProductServiceApiV2.java
@@ -0,0 +1,63 @@
+package com.musinsam.productservice.application.service.V2;
+
+import com.musinsam.common.user.CurrentUserDtoApiV1;
+import com.musinsam.productservice.application.dto.request.ReqProductPatchByProductIdDtoApiV1;
+import com.musinsam.productservice.application.dto.request.ReqProductPostDtoApiV1;
+import com.musinsam.productservice.application.dto.request.ReqProductPutByProductIdDtoApiV1;
+import com.musinsam.productservice.application.dto.response.ResProductGetByProductIdDtoApiV1;
+import com.musinsam.productservice.application.dto.response.ResProductGetDtoApiV1;
+import com.musinsam.productservice.application.dto.response.ResProductGetStockDtoApiV1;
+import com.musinsam.productservice.domain.product.vo.ProductStatus;
+import com.musinsam.productservice.infrastructure.dto.res.ReqProductSaveProductsDtoApiV1;
+import com.musinsam.productservice.infrastructure.dto.res.ResProductInfoGetByProductId;
+import com.musinsam.productservice.infrastructure.s3.S3Folder;
+import jakarta.validation.Valid;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ProductServiceApiV2 {
+
+  void createProduct(CurrentUserDtoApiV1 currentUser, @Valid ReqProductPostDtoApiV1 dto,
+      List<MultipartFile> images);
+
+  ResProductGetByProductIdDtoApiV1 getById(UUID productId);
+
+  ResProductGetDtoApiV1 getProductList(BigDecimal minPrice,
+      BigDecimal maxPrice,
+      ProductStatus status,
+      String sortBy,
+      int page,
+      int size);
+
+  void updateProduct(CurrentUserDtoApiV1 currentUser, UUID productId,
+      @Valid ReqProductPutByProductIdDtoApiV1 dto,
+      List<MultipartFile> images);
+
+  void deleteProduct(CurrentUserDtoApiV1 currentUser, UUID productId);
+
+  ResProductGetStockDtoApiV1 getProductStock(CurrentUserDtoApiV1 currentUser, UUID productId);
+
+  void updateProductStock(CurrentUserDtoApiV1 currentUser, UUID productId,
+      @Valid ReqProductPatchByProductIdDtoApiV1 dto);
+
+  void saveImageFile(S3Folder s3Folder, MultipartFile file, UUID id);
+
+  void saveProductImageInfo(final S3Folder s3Folder,
+      final MultipartFile file,
+      final UUID id,
+      final String fileName);
+
+  Boolean checkAndReduceStock(UUID productId, Integer quantity);
+
+  void restoreStock(UUID productId, Integer quantity);
+
+  ResProductInfoGetByProductId getProductInfo(UUID productId);
+
+  void saveProductsToRedis(ReqProductSaveProductsDtoApiV1 dto);
+
+  void updateDiscountRate(UUID productId, Integer discountRate);
+
+  void deleteEventProduct(UUID productId);
+}
